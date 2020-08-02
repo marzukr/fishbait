3c84202dcad7072db51d41b0549e9de6e8917af1
Marzuk Rashid <marzukr@gmail.com>
2020-08-02 00:37:40 -0500

    deck shuffle

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <random>
 #include "SevenEval.h"
+#include "Constants.h"
+
+const int num_players = 6;
 
 void MCCFR_P(int T) {
     
 }
 
 int main() {
-    std::cout << "All in.\n";
-    std::cout << SevenEval::GetRank(0, 4, 8, 12, 16, 20, 24) << std::endl;
+    int deck[DECK_SIZE];
+
+    for (int i = 0; i < DECK_SIZE; i++) {
+        deck[i] = i;
+    }
+   
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    for (int i = DECK_SIZE - 1; i >= 1; i--) {
+        std::uniform_int_distribution<std::mt19937::result_type> dist6(0,i);
+        int j = dist6(rng);
+        int temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+
+    for (int i = 0; i < DECK_SIZE; i++) {
+        std::cout << deck[i] << std::endl;
+    }
+
+    // std::cout << SevenEval::GetRank(0, 4, 8, 12, 16, 20, 24) << std::endl;
     return 0;
 }
