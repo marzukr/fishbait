4139b77bfaa76fa116ffc8a75aa81c0f641d3666
Marzuk Rashid <marzukr@gmail.com>
2020-12-29 01:18:30 -0600

    elkan data structures

--- src/abstraction/cluster.cpp
+++ src/abstraction/cluster.cpp
@@ -69,9 +69,15 @@ class HandData {
         ~HandData() { delete[] data_; }
 
         virtual double Distance(uint32_t p, uint32_t q) = 0;
+        virtual double Distance(uint32_t p, const T* q) = 0;
+        virtual double Distance(const T* p, const T* q) = 0;
 
         T data(uint32_t i, uint16_t j) {
             return data_[i*binSize_ + j];
+        }
+
+        const T* data_ptr(uint32_t i, uint16_t j) {
+            return &data_[i*binSize_ + j];
         }
 
         uint32_t nDocuments() { return nDocuments_; }
@@ -90,18 +96,26 @@ class EHSDistribution: public HandData<T> {
             : HandData<T>(data_array, n_documents, bin_size)
         {}
 
-        double Distance(uint32_t p, uint32_t q) {
+        double Distance(const T* p, const T* q) {
             double prev = 0;
             double sum = 0;
             for(uint8_t i = 1; i < this->binSize_ + 1; i++) {
                 // all flops have area of 1081
-                T p_i = this->data(p, i-1);
-                T q_i = this->data(q, i-1);
+                T p_i = p[i-1];
+                T q_i = q[i-1];
                 double nxt = p_i + prev - q_i;
                 sum += std::abs(nxt);
                 prev = nxt;
             }
             return sum;
+        }
+
+        double Distance(uint32_t p, uint32_t q) {
+            return Distance(this->data_ptr(p, 0), this->data_ptr(q, 0));
+        }
+
+        double Distance(uint32_t p, const T* q) {
+            return Distance(this->data_ptr(p, 0), q);
         }
 };
 
@@ -110,9 +124,73 @@ class KMeans {
     public:
         KMeans(HandData<T>* data, uint32_t k) : data_(data), k_(k) {
             clusters_ = new T[k*data_->binSize()];
+            assignments_ = new uint32_t[data_->nDocuments()];
         }
         ~KMeans() { delete[] clusters_; }
 
+        void Elkan(bool verbose = false) {
+            InitPlusPlus(verbose);
+
+            double* lower_bounds = new double[data_->nDocuments()*k_];
+            auto lower_bound = [&lower_bounds, this](
+                    uint32_t x, uint32_t c) -> double& { 
+                return lower_bounds[x*this->k_ + c];
+            };
+            for(uint32_t x = 0; x < data_->nDocuments(); x++) {
+                for(uint32_t c = 0; c < k_; c++) {
+                    lower_bound(x,c) = 0;
+                }
+            }
+            if(verbose) {
+                std::cout << "initialized lower bounds" << std::endl;
+            }
+
+            double* cluster_dists = new double[(k_*k_-k_)/2];
+            auto cluster_dist = [&cluster_dists, this](
+                    uint32_t c1, uint32_t c2) -> double& { 
+                uint32_t idx = c1*this->k_ - c1*(c1+1)/2 + c2 - c1 - 1;
+                return cluster_dists[idx];
+            };
+            for(uint32_t c1 = 0; c1 < k_; c1++) {
+                for(uint32_t c2 = c1 + 1; c2 < k_; c2++) {
+                    T* c1_ptr = cluster_ptr(c1, 0);
+                    T* c2_ptr = cluster_ptr(c2, 0);
+                    cluster_dist(c1, c2) = data_->Distance(c1_ptr, c2_ptr);
+                }
+            }
+            if(verbose) {
+                std::cout << "calculated cluster distances" << std::endl;
+            }
+
+            double* upper_bounds = new double[data_->nDocuments()];
+            for(uint32_t x = 0; x < data_->nDocuments(); x++) {
+                uint32_t& c = assignments_[x];
+                for(uint32_t cprime = 0; cprime < k_; cprime++) {
+                    bool calc_x_cprime_dist = false;
+                    if(cprime != 0) {
+                        double c_cprime_dist = cluster_dist(c, cprime);
+                        double x_c_dist = upper_bounds[x];
+                        if(c_cprime_dist/2 < x_c_dist) {
+                            calc_x_cprime_dist = true;
+                        }
+                    }
+                    if(cprime == 0 || calc_x_cprime_dist) {
+                        T* cprime_ptr = cluster_ptr(cprime, 0);
+                        double x_cprime_dist = data_->Distance(x, cprime_ptr);
+                        lower_bound(x,cprime) = x_cprime_dist;
+                        if(cprime == 0 || x_cprime_dist < upper_bounds[x]) {
+                            assignments_[x] = cprime;
+                            upper_bounds[x] = x_cprime_dist;
+                        }
+                    }
+                }
+            }
+            if(verbose) {
+                std::cout << "computed initial assignments and upper bounds";
+                std::cout << std::endl;
+            }
+        }
+    private:
         void InitPlusPlus(bool verbose = false) {
             std::random_device dev;
             std::mt19937 rng(dev());
@@ -165,10 +243,15 @@ class KMeans {
             delete[] squared_dists;
             delete[] clusters;
         }
-    private:
+
+        T* cluster_ptr(uint32_t c, uint16_t j) {
+            return &clusters_[c*data_->binSize() + j];
+        }
+
         HandData<T>* data_;
         const uint32_t k_;
         T* clusters_;
+        uint32_t* assignments_;
 };
 
 int main() {
@@ -184,7 +267,7 @@ int main() {
     );
 
     KMeans k(e, 200);
-    k.InitPlusPlus(true);
+    k.Elkan(true);
     // std::cout << "Done." << std::endl;
     // // should be 36
     // std::cout << histograms[276*BIN_SIZE + 29] << std::endl;
