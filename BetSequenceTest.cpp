4b8621f47c3ec6dbd3621015a914d6d75aeeb9b4
Marzuk Rashid <marzukr@gmail.com>
2020-12-30 01:06:01 -0600

    removed bad files, ignored .DS_Store

--- BetSequenceTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "BetSequence.h"
-
-int main(){
-    float raise_sizes [1] = {1};
-    char num_raise_sizes = 1;
-    float starting_chips = 2;
-    std::cout << GetNumPossible(raise_sizes, num_raise_sizes, starting_chips) << std::endl;
-    return 0;
-}
