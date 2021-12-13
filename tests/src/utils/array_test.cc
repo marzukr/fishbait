943a18d36f52ec140f446413f5c245ed5012c0d7
Marzuk Rashid <mail@marzuk.io>
2021-12-12 20:53:07 -0600

    array filler function and test

---
+++ tests/src/utils/array_test.cc
@@ -0,0 +1,13 @@
+// Copyright 2021 Marzuk Rashid
+
+#include <array>
+
+#include "catch2/catch.hpp"
+#include "utils/array.h"
+
+TEST_CASE("array fill test", "[utils][array]") {
+  std::array<int, 15> test_arr = fishbait::FilledArray<int, 15>(256);
+  for (auto it = test_arr.begin(); it != test_arr.end(); ++it) {
+    REQUIRE(*it == 256);
+  }
+}  // TEST_CASE "array fill test"
