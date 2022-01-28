52ea5226496bb91f7388785b65bde53abdbfadb5
Marzuk Rashid <mail@marzuk.io>
2022-01-28 00:13:03 +0000

    avg_to_hd5 help message

--- src/relay/avg_to_hd5.cc
+++ src/relay/avg_to_hd5.cc
@@ -1,5 +1,7 @@
 // Copyright 2021 Marzuk Rashid
 
+#include <iostream>
+
 #include "blueprint/definitions.h"
 #include "blueprint/hyperparameters.h"
 #include "blueprint/strategy.h"
@@ -8,7 +10,12 @@
 #include "poker/node.h"
 #include "relay/scribe.h"
 
-int main([[maybe_unused]] int argc, char** argv) {
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    std::cout << "Usage: " << argv[0] << " <average location> "
+              << "<hdf save location>" << std::endl;
+    return 1;
+  }
   using StrategyT = fishbait::Strategy<fishbait::hparam::kPlayers,
                                        fishbait::hparam::kActions,
                                        fishbait::ClusterTable>;
@@ -16,4 +23,5 @@ int main([[maybe_unused]] int argc, char** argv) {
   AverageT avg = AverageT::LoadAverage(argv[1], true);
   fishbait::Scribe<fishbait::hparam::kPlayers, fishbait::hparam::kActions,
                    fishbait::ClusterTable> ahmes{avg, argv[2], true};
+  return 0;
 }
