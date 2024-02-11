0dcbf335653614912fd62f5d50b95f388035ab85
Marzuk Rashid <mail@marzuk.io>
2024-02-10 18:12:56 -0600

    Datadog logs

--- src/api/src/utils.py
+++ src/api/src/utils.py
@@ -1,11 +1,20 @@
 """Utils for the API."""
 
 import logging
+import ddtrace
 
 logging.basicConfig(
   level=logging.DEBUG,
-  format='%(levelname)s:%(name)s:%(lineno)d: %(message)s'
+  format=(
+    '%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+    '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+    '- %(message)s'
+  )
 )
 
 def get_logger(name: str):
   return logging.getLogger(name)
+
+@ddtrace.tracer.wrap()
+def hello():
+    get_logger(__name__).info('Hello, World!')
