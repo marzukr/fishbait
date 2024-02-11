0ac2e62003d5a25a82f70a770747180f259697b5
Marzuk Rashid <mail@marzuk.io>
2024-02-10 19:05:07 -0600

    Log to file

--- src/api/src/utils.py
+++ src/api/src/utils.py
@@ -1,7 +1,18 @@
 """Utils for the API."""
 
 import logging
+from logging.handlers import RotatingFileHandler
 import ddtrace
+
+import settings
+
+file_handler = RotatingFileHandler(
+  settings.LOG_LOCATION,
+  mode='a',
+  maxBytes=5*1024*1024, 
+  backupCount=2,
+  encoding=None,
+)
 
 logging.basicConfig(
   level=logging.DEBUG,
@@ -9,7 +20,8 @@ logging.basicConfig(
     '%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
     '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
     '- %(message)s'
-  )
+  ),
+  handlers=[file_handler],
 )
 
 def get_logger(name: str):
